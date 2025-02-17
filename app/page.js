1281beddbad99bbd90fcd0f3ce9a5351bf5c4fbc
Jacob May <142454161+Ja-ke-May@users.noreply.github.com>
2025-02-17 21:47:44 +0000

    google tag

--- app/page.js
+++ app/page.js
@@ -4,25 +4,29 @@
 import FullPage from "./components/fullpage"
 import { useEffect } from "react";
 
-useEffect(() => {
-  const script = document.createElement('script');
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZG3EYE9D4F';
-  script.async = true;
-  document.body.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZG3EYE9D4F');
-
-  return () => {
-    document.body.removeChild(script);
-  };
-}, []);
 
 
 export default function Home() {
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZG3EYE9D4F';
+    script.async = true;
+    document.body.appendChild(script);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-ZG3EYE9D4F');
+  
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  
+  
   return (
     <div>
       <FullPage />
